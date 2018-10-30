b8e0018d38855cf79edaeb04d590c46672c49ec9
saketh varma <sakethbobby@gmail.com>
2018-10-30 13:27:06 -0400

    filter branches #1

--- logistic_model.py
+++ logistic_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-# import tensorflow as tf
+import tensorflow as tf
 
 def get_train_val_test_split(X):
     print("Splitting data into training, validation and test sets")
